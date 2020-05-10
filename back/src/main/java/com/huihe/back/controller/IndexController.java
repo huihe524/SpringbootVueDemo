2ffaf51f272716fb75a7e13eb7b8c66acb601004
HANXU <1076998404@qq.com>
2020-05-10 15:24:41 +0800

    change @RestController
    @RestController注解相当于@ResponseBody ＋ @Controller合在一起的作用

--- back/src/main/java/com/huihe/back/controller/IndexController.java
+++ back/src/main/java/com/huihe/back/controller/IndexController.java
@@ -3,8 +3,9 @@ package com.huihe.back.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
     @GetMapping("/index")
