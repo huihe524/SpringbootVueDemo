41cc87472ddb3779f296d3ed9cc63e3e24fc23f0
HANXU <1076998404@qq.com>
2020-05-11 20:56:02 +0800

    Configure axios to use the @crossorigin annotation
    配置 axios 使用@CrossOrigin 注解

--- back/src/main/java/com/huihe/back/controller/IndexController.java
+++ back/src/main/java/com/huihe/back/controller/IndexController.java
@@ -1,18 +1,27 @@
 package com.huihe.back.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-
-    @GetMapping("/index")
+    @CrossOrigin
+    @GetMapping("/api/index")
     @ResponseBody //将返回的字符串进行解析在前端显示
     //@ResponseBody的作用其实是将java对象转为json格式的数据。
     public String index(){
         System.out.println("Hello World");
         return "Hello World";
+    }
+    @CrossOrigin
+    @GetMapping("/api/login")
+    @ResponseBody //将返回的字符串进行解析在前端显示
+    //@ResponseBody的作用其实是将java对象转为json格式的数据。
+    public String login(){
+        System.out.println("Hello login");
+        return "Hello login";
     }
 }
--- front/front/src/main.js
+++ front/front/src/main.js
@@ -1,9 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+// 设置反向代理，前端请求默认发送到 http://localhost:8443/api
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8181/api'
+// 全局注册，之后可在其他组件中通过 this.$axios 发送数据
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
